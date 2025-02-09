b34e7915dc0982d257d8db8cad6b1508314996b2
Migueluckleo <migueluxleo@gmail.com>
2025-02-08 19:51:12 -0600

    circulos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   content: ["./*.html", "./**/*.html", "./js/**/*.js"],
   theme: {
-    extend: {
-      fontFamily: {
-        space: ["Space Grotesk", "sans-serif"],
+      extend: {
+          keyframes: {
+              blob: {
+                  "0%": { transform: "translate(0px, 0px) scale(1)" },
+                  "33%": { transform: "translate(30px, -50px) scale(1.2)" },
+                  "66%": { transform: "translate(20px, 20px) scale(.8)" },
+                  "100%": { transform: "translate(0px, 0px) scale(1)" },
+              },
+          },
+          animation: {
+              blob: "blob 7s infinite",
+          },
+          fontFamily: {
+              space: ["Space Grotesk", "sans-serif"],
+          },
       },
-    },
   },
   plugins: [],
 };
